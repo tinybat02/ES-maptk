90713d3f6b182e8bc60ade7df842501c7318ee77
Nam Le <nam.leduc02@gmail.com>
2020-12-07 11:54:06 +0100

    revert back tile server

--- dist/module.js
+++ dist/module.js
@@ -100677,11 +100677,9 @@ function (_super) {
       pitch: 56,
       bearing: 60,
       baseLayer: new maptalks__WEBPACK_IMPORTED_MODULE_2__["TileLayer"]('base', {
-        // urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-        // subdomains: ['a', 'b', 'c', 'd'],
-        urlTemplate: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        subdomains: ['a', 'b', 'c'],
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+        subdomains: ['a', 'b', 'c', 'd'],
+        attribution: '&copy; <a href="https://carto.com/">CARTO</a>'
       }),
       touchGesture: true,
       doubleClickZoom: false
